243c04f2299d49c2f12f286c561a5d5c35496275
JeongSeok <97824309+RryNoel@users.noreply.github.com>
2024-04-29 11:27:06 +0900

    List Item

--- SpartaDungeon/Program.cs
+++ SpartaDungeon/Program.cs
@@ -17,6 +17,16 @@ namespace SpartaDungeon
     {
         private Character player;
 
+        List<Item> shopItems = new List<Item>
+            {
+                new Item("수련자 갑옷", "방어력 +", 5, "수련에 도움을 주는 갑옷입니다.", 1000),
+                new Item("무쇠 갑옷", "방어력 +", 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 2000),
+                new Item("스파르타의 갑옷", "방어력 +", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500),
+                new Item("낡은 검", "공격력 +", 2, "쉽게 볼 수 있는 낡은 검입니다.", 600),
+                new Item("청동 도끼", "공격력 +", 5, "어디선가 사용됐던거 같은 도끼입니다.", 1500),
+                new Item("스파르타의 창", "공격력 +", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2500),
+            };
+
         // 게임 매니저 생성자에서 함수 실행
         public GameManager()
         {
@@ -26,6 +36,8 @@ namespace SpartaDungeon
         private void InitiallizeGame()
         {
             player = new Character("Diluc", "전사", 1, 10, 5, 100, 15000);
+
+            
         }
 
         public void StartGame()
@@ -278,17 +290,6 @@ namespace SpartaDungeon
             Console.WriteLine("\n");
             Console.WriteLine("[아이템 목록]");
             Console.WriteLine("\n");
-
-            // 상점에 아이템 추가
-            List<Item> shopItems = new List<Item>
-            {
-                new Item("수련자 갑옷", "방어력 +", 5, "수련에 도움을 주는 갑옷입니다.", 1000),
-                new Item("무쇠 갑옷", "방어력 +", 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 2000),
-                new Item("스파르타의 갑옷", "방어력 +", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500),
-                new Item("낡은 검", "공격력 +", 2, "쉽게 볼 수 있는 낡은 검입니다.", 600),
-                new Item("청동 도끼", "공격력 +", 5, "어디선가 사용됐던거 같은 도끼입니다.", 1500),
-                new Item("스파르타의 창", "공격력 +", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2500),
-            };
 
             for (int i = 0; i < shopItems.Count; i++)
             {
